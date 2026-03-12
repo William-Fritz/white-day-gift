b7a991b51849e0d802e74f9a98f54bb49038c32b
splizert <splizert@gmail.com>
2026-03-13 00:08:26 +0700

    final path sync

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/white-day/',
+  base: '/white-day-gift/',
   plugins: [react()],
 })
+git remote set - url origin https://github.com/William-Fritz/white-day.git
